a3164d4765dc659ce33e8c6224f4b181cc712678
thierry hentic <thierry.hentic@gmail.com>
2023-04-18 16:42:08 +0200

    Initial Commit

---
+++ Financial/Document.swift
@@ -0,0 +1,28 @@
+//
+//  Document.swift
+//  Financial
+//
+//  Created by thierry hentic on 18/04/2023.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
---
+++ Financial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Financial
+//
+//  Created by thierry hentic on 18/04/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
